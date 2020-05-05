e11fbefef9a6270ef439f0a850ad1eae7a68d2f4
Greg Trihus <greg_trihus@sil.org>
2020-05-05 11:50:25 -0500

    Make custom login complete
    Add documentation link

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -24,23 +24,40 @@
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
-Cypress.Commands.add('login', (overrides = {}) => {
-    Cypress.log({
-      name: 'loginViaAuth0',
-    });
-  
-    const options = {
-      method: 'POST',
-      url: Cypress.env('auth_url'),
-      body: {
-        grant_type: 'password',
-        username: Cypress.env('auth_username'),
-        password: Cypress.env('auth_password'),
-        audience: Cypress.env('auth_audience'),
-        scope: 'openid profile email',
-        client_id: Cypress.env('auth_client_id'),
-        client_secret: Cypress.env('auth_client_secret'),
-      },
-    };
-    cy.request(options);
+Cypress.Commands.add("login", (overrides = {}) => {
+  Cypress.log({
+    name: "loginViaAuth0",
   });
+
+  const options = {
+    method: "POST",
+    url: Cypress.env("auth_url"),
+    body: {
+      grant_type: "password",
+      username: Cypress.env("auth_username"),
+      password: Cypress.env("auth_password"),
+      audience: Cypress.env("auth_audience"),
+      scope: "openid profile email",
+      client_id: Cypress.env("auth_client_id"),
+      client_secret: Cypress.env("auth_client_secret"),
+    },
+  };
+  cy.request(options)
+    .then((resp) => {
+      return resp.body;
+    })
+    .then((body) => {
+      const { access_token, expires_in, id_token } = body;
+      const auth0State = {
+        nonce: "",
+        state: "some-random-state",
+      };
+      const callbackUrl = `http://localhost:3000/callback#access_token=${access_token}&scope=openid&id_token=${id_token}&expires_in=${expires_in}&token_type=Bearer&state=${auth0State.state}`;
+      cy.visit(callbackUrl, {
+        onBeforeLoad(win) {
+          win.document.cookie =
+            "com.auth0.auth.some-random-state=" + JSON.stringify(auth0State);
+        },
+      });
+    });
+});
--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,5 +1,7 @@
+// See: https://stackoverflow.com/questions/51694218/cypress-custom-typescript-command-is-not-a-function
+
 declare namespace Cypress {
   interface Chainable<Subject = any> {
-    login(): Chainable<Cypress.Response>;
+    login(): Chainable;
   }
 }
